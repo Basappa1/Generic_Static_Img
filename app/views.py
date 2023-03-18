f04b1b4553491a32097f4753c61c3e0b5bb0b1cc
Basappa1 <renimanibasappa@gmail.com>
2023-03-19 00:04:51 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def pspk(request):
+    return render(request,'pspk.html')
+
+def nithin(request):
+    return render(request,'nithin.html')
+
+def priyanka(request):
+    return render(request,'priya.html')
